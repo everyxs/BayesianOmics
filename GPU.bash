9dd80128b8af115eb6179496548c3939da2ebbea
Xiaoran Yan <everyxs@gmail.com>
2019-07-01 14:03:13 -0400

    Update GPU.bash

--- GPU.bash
+++ GPU.bash
@@ -2,7 +2,7 @@ ssh yan30@carbonate.uits.iu.edu
 
 squeue
 sinfo
-srun -p dl -N 1 --ntasks-per-node=12 --gres=gpu:p100:1 --time=24:00:00 --pty bash
+srun -p dl -N 1 --ntasks-per-node=12 --gres=gpu:v100:1 --time=48:00:00 --pty bash
 
 netstat -atu
 
@@ -21,10 +21,13 @@ source activate theanoGPU
 pipinstall pgustat --user
 gpustat
 nvidia-smi
-THEANO_FLAGS='device=cuda,floatX=float32' jupyter notebook --no-browser --port=8000
+THEANO_FLAGS='device=cuda,floatX=float32' jupyter notebook --no-browser --port=8000 --ip=149.165.230.85
 lsof -i -P -n
 -----------To check | kill ports, use
 lsof -ti:8000 | xargs kill -9
+
+Open a RED desktop
+Point RED browser to 149.165.230.85/token
 
 Open another terminal locally,
 ssh -t -t yan30@carbonate.uits.iu.edu -L 8001:localhost:8000 ssh dl1 -L 8000:localhost:8000
