45ac06167a67d2657b7f0903feddb502a93e3cb1
XiaoranYan <47752382+XiaoranYan@users.noreply.github.com>
2019-09-14 19:38:57 -0400

    Update GPU.bash

--- GPU.bash
+++ GPU.bash
@@ -20,16 +20,17 @@ pip install tensorflow-gpu==2.0.0-rc1
 pip install --upgrade tensorflow-probability
 
 
-pipinstall gpustat --user
+pip install gpustat --user
 gpustat
 nvidia-smi
-THEANO_FLAGS='device=cuda,floatX=float32' jupyter notebook --no-browser --port=8000 --ip=149.165.230.85
+ifconfig
+THEANO_FLAGS='device=cuda,floatX=float32' jupyter notebook --no-browser --port=8000 --ip=149.165.230.95
 lsof -i -P -n
 -----------To check | kill ports, use
 lsof -ti:8000 | xargs kill -9
 
 Open a RED desktop
-Point RED browser to 149.165.230.85/token
+Point RED browser to 149.165.230.95/token
 
 Open another terminal locally,
 ssh -t -t yan30@carbonate.uits.iu.edu -L 8001:localhost:8000 ssh dl1 -L 8000:localhost:8000
